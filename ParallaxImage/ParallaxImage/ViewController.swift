0d6e0d7cc7767861dafc8afdbe0e1fda3e27ef1b
vruf <143929626+vruf@users.noreply.github.com>
2023-09-21 15:01:05 +0300

    Update ViewController.swift

--- ParallaxImage/ParallaxImage/ViewController.swift
+++ ParallaxImage/ParallaxImage/ViewController.swift
@@ -66,7 +66,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             imageViewHeightAnchor.constant = imageHeight - contentOffsetheight
         }
         
-        scrollView.verticalScrollIndicatorInsets.top = imageViewHeightAnchor.constant - view.safeAreaInsets.top
+        scrollView.verticalScrollIndicatorInsets.top = imageViewHeightAnchor.constant
     }
     
 }
